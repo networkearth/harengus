fe4d8af700d0ec550897a54c91d3e8f2c959a405
erebuseternal <marcelsanders96@gmail.com>
2023-09-30 09:33:35 -0400

    a bot

---
+++ app/bot/app.py
@@ -0,0 +1,21 @@
+import awsgi
+ 
+from flask import Flask, render_template
+from flaskext.markdown import Markdown
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+Bootstrap(app)
+Markdown(app)
+
+ENVIRONMENT = "/" #"/Prod" # "/Stage"
+
+@app.route("/index")
+def index():
+    return render_template('index.html', environment=ENVIRONMENT)
+
+def lambda_handler(event, context):
+    base64_content_types = ["image/png"]
+    response = awsgi.response(app, event, context, base64_content_types)
+    return response
